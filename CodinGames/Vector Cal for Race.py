7e6af5892fa3876e348277f51dc682aa798fc084
SebNik <lord.voldmeort1492@gmail.com>
2020-09-13 16:43:39 +0200

    create route test and dummy of plot data

--- CodinGames/Vector Cal for Race.py
+++ CodinGames/Vector Cal for Race.py
@@ -16,35 +16,44 @@ def generate_map(checkpoint_count=3, boundaries=9000, radius_checkpoint=600):
     return checkpoints_coordinates, start_point, start_vector
 
 
-# x = np.array([23, 23.5, 24, 25, 25, 23])
-# y = np.array([8, 9, 11, 12, 13, 12])
-#
-# # append the starting x,y coordinates
-# x = np.r_[x, x[0]]
-# y = np.r_[y, y[0]]
-#
-# # fit splines to x=f(u) and y=g(u), treating both as periodic. also note that s=0
-# # is needed in order to force the spline fit to pass through all the input points.
-# tck, u = interpolate.splprep([x, y], s=0, per=True)
-#
-# # evaluate the spline fits for 1000 evenly spaced distance values
-# xi, yi = interpolate.splev(np.linspace(0, 1, 50), tck)
-#
-# # plot the result
-# fig, ax = plt.subplots(1, 1)
-# ax.plot(x, y, 'or', label='points')
-# ax.plot(xi, yi, '-b', label='line')
-#
-# soa = np.array([[23, 8, 0.5, 1], [24, 11, 1, 1]])
-# X, Y, U, V = zip(*soa)
-#
-# ax.quiver(X, Y, U, V, angles='xy', scale_units='xy', scale=1, width=0.008, color='lawngreen')
-#
-#
-# plt.legend()
-# plt.draw()
-# plt.show()
+def create_route(checkpoints_coordinates, start_point, start_vector):
+    # this function will create the route
+    x = np.r_[checkpoints_coordinates[:, 0], checkpoints_coordinates[0, 0]]
+    y = np.r_[checkpoints_coordinates[:, 1], checkpoints_coordinates[0, 1]]
+    # print(x, y)
+    # fit splines to x=f(u) and y=g(u), treating both as periodic. also note that s=0
+    # is needed in order to force the spline fit to pass through all the input points.
+    tck, u = interpolate.splprep([x, y], s=0, per=True)
+    # evaluate the spline fits for 1000 evenly spaced distance values
+    xi, yi = interpolate.splev(np.linspace(0, 1, 50), tck)
+    # creating vectors dummy
+    vectors = np.array([[23, 8, 0.5, 1], [24, 11, 1, 1]])
+    # retuning the generated route
+    return checkpoints_coordinates, start_point, start_vector, xi, yi, vectors
+
+
+def plot_data(checkpoints_coordinates, start_point, start_vector, xi, yi, vectors):
+    # plotting the data
+    # plot the result
+    fig, ax = plt.subplots(1, 1)
+    ax.plot(checkpoints_coordinates[:, 0], checkpoints_coordinates[:, 1], 'or', label='points')
+    ax.plot(xi, yi, '-b', label='line')
+
+    soa = np.array([[23, 8, 0.5, 1], [24, 11, 1, 1]])
+    X, Y, U, V = zip(*soa)
+
+    ax.quiver(X, Y, U, V, angles='xy', scale_units='xy', scale=1, width=0.008, color='lawngreen')
+
+    plt.legend()
+    plt.draw()
+    plt.show()
+
 
 if __name__ == '__main__':
     # start code
-    print(generate_map())
+    coor, s_p, s_v = generate_map()
+    print(coor, s_p, s_v)
+    # generating the route
+    c_c, s_p, s_v, x, y, v = create_route(coor, s_p, s_v)
+    # plotting the data
+    plot_data(c_c, s_p, s_v, x, y, v)
